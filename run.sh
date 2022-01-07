e93f96997b04b242f8915fe1d4b38fb197221579
Marco Della Rocca <marco741147@gmail.com>
2022-01-07 21:43:48 +0100

    Added validation set in run sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ python align_labels.py && \
 python split_labels_by_class.py --joined_ds && \
 python create_dataset_split.py --joined_ds --im_path utkface && \
 python split_labels_by_class.py && \
-python create_dataset_split.py --im_path utkface && \
+python create_dataset_split.py --val_sr 0.1 --im_path utkface && \
 python create_balanced_datasets.py
